1eeae5d006b19d758f03dc2ccc22e573abba1495
Kevin Jonathan <kevinjonathan2701@gmail.com>
2019-02-10 22:24:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { Layout } from 'antd'
+import { Layout, Menu, Breadcrumb } from 'antd'
 import 'antd/dist/antd.css';
 
 import Navigation from './components/Navigation';
@@ -20,8 +20,18 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Layout className="App">
-          <Header style={HeaderStyles} >
-            <h1>Header</h1>
+          <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
           </Header>
           <Content style={ContentStyles}>
             <Navigation />
