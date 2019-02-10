fdf777c9a15d539f8e7ffe660d4b8639b22a9d6e
Adhitama Fikri <adhitama.fikri@ralali.com>
2019-02-10 18:28:04 +0700

    updates, now using antd Layout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Layout } from 'antd'
 import 'antd/dist/antd.css';
 
-import Header from './components/Header';
-import Main from './components/Main';
 import Navigation from './components/Navigation';
 import Competition from './components/Page/Competition';
 import Festival from './components/Page/Festival';
 import BigResto from './components/Page/BigResto';
 
+import HeaderStyles from './components/styles/HeaderStyles';
+import ContentStyles from './components/styles/ContentStyles';
+
+const {
+  Header, Content
+} = Layout;
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-          <Navigation />
-          <Header />
-          <Main>
+        <Layout className="App">
+          <Header style={HeaderStyles} >
+            <h1>Header</h1>
+          </Header>
+          <Content style={ContentStyles}>
+            <Navigation />
             <Route exact path="/" component={Festival} />
             <Route path="/competition" component={Competition} />
             <Route path="/big-resto" component={BigResto} />
-          </Main>
-        </div>
+          </Content>
+        </Layout>
       </BrowserRouter>
     );
   }
---
+++ src/components/styles/ContentStyles.js
@@ -0,0 +1,5 @@
+import { HEADER_HEIGHT } from '../StyleVariables';
+
+export default {
+  marginTop: HEADER_HEIGHT
+};
