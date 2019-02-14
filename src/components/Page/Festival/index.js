dc2d7d24e624b07bf60957b07179399bf53f6eb6
logustra <faizal.putra@ralali.com>
2019-02-14 16:46:19 +0700

    add foods on page festival

--- src/components/Page/Festival/index.js
+++ src/components/Page/Festival/index.js
@@ -5,35 +5,50 @@ import {
     Col
 } from 'reactstrap'
 
-import {
-    StyledHero,
-    StyledFood
-} from './style'
+import StyledFestival from './style'
 
 import ImgLogo from '../../../assets/Festival/logo.png'
+import ImgFoods from '../../../assets/Festival/foods.png'
+import ImgFoodsDecorationTop from '../../../assets/Festival/cloud-grey.png'
+import ImgFoodsDecorationBottom from '../../../assets/Festival/catoon.png'
 
 const Festival = () => (
     <React.Fragment>
-        <StyledHero>
-            <div className="hero-content-container">
-                <div className="hero-clip-path"></div>
-                <div className="hero-content">
+        <StyledFestival>
+            <section className="hero">
+                <div className="hero-content-container">
+                    <div className="hero-clip-path"></div>
+                    <div className="hero-content">
+                        <Container>
+                            <Row className="align-items-center">
+                                <Col md="6">
+                                    <img className="img-fluid mx-auto d-block" src={ImgLogo} alt="Festival" />
+                                </Col>
+                                <Col md="6">
+                                    Dolor eu cupidatat dolore aliquip occaecat magna id cupidatat. Nostrud minim enim excepteur Lorem nisi voluptate. Sit aute ex commodo cupidatat ullamco. Consequat dolor eu Lorem laboris eu. Nostrud aute labore amet excepteur consectetur ipsum ipsum esse officia occaecat est laborum incididunt. Id mollit in consectetur quis cillum culpa aliquip aliqua. Anim ut excepteur labore proident laboris.
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </div>
+            </section>
+            <section className="foods">
+                <div className="foods-content-container">
                     <Container>
-                        <Row className="align-items-center">
+                        <Row>
                             <Col md="6">
-                                <img className="img-fluid" src={ImgLogo} alt="Festival" />
+                                Irure qui eiusmod veniam qui anim consequat enim. Velit aliqua tempor veniam ullamco irure laboris officia ea. Qui ex dolor ipsum veniam occaecat anim elit. Laborum aliqua reprehenderit elit duis ut veniam consectetur sint ut. Deserunt amet exercitation amet enim ex aute ipsum. Elit magna sunt amet amet irure do qui. Occaecat consectetur fugiat pariatur laboris enim.
                             </Col>
-                            <Col md="6">
-                                Dolor eu cupidatat dolore aliquip occaecat magna id cupidatat. Nostrud minim enim excepteur Lorem nisi voluptate. Sit aute ex commodo cupidatat ullamco. Consequat dolor eu Lorem laboris eu. Nostrud aute labore amet excepteur consectetur ipsum ipsum esse officia occaecat est laborum incididunt. Id mollit in consectetur quis cillum culpa aliquip aliqua. Anim ut excepteur labore proident laboris.
+                            <Col>
+                                <img className="img-fluid mx-auto d-block" src={ImgFoods} alt="Foods" />
                             </Col>
                         </Row>
                     </Container>
+                    <img className="img-fluid foods-decoration-top" src={ImgFoodsDecorationTop} alt="Decoration" />
+                    <img className="img-fluid foods-decoration-bottom" src={ImgFoodsDecorationBottom} alt="Decoration " />
                 </div>
-            </div>
-        </StyledHero>
-        <StyledFood>
-            Food
-        </StyledFood>
+            </section>
+        </StyledFestival>
     </React.Fragment>
 )
 
--- src/components/Page/Festival/style.js
+++ src/components/Page/Festival/style.js
@@ -3,12 +3,16 @@ import {rgba} from 'polished'
 
 import ImgHero from '../../../assets/Festival/banner.png'
 
-const StyledHero = Styled.section`
-    background: linear-gradient( ${rgba('white', .8)}, ${rgba('white', .8)}), url(${ImgHero}) center center no-repeat;
-    background-size: cover;
-    height: 650px;
-    width: 100%;
-    clip-path: polygon(0 0, 100% 0, 100% 80%, 0 100%);
+const StyledFestival = Styled.main`
+    overflow: hidden;
+    font-size: 19px;
+
+    .hero {
+        background: linear-gradient( ${rgba('white', .8)}, ${rgba('white', .8)}), url(${ImgHero}) center center no-repeat;
+        background-size: cover;
+        height: 650px;
+        width: 100%;
+        clip-path: polygon(0 0, 100% 0, 100% 80%, 0 100%);
 
         .hero-content-container {
             position: relative;
@@ -18,7 +22,7 @@ const StyledHero = Styled.section`
         }
 
         .hero-clip-path {
-            background: linear-gradient( ${rgba('red', .8)}, ${rgba('red', .8)});
+            background: linear-gradient( ${rgba('#ed1e79', .8)}, ${rgba('#ed1e79', .8)});
             height: 500px;
             clip-path: polygon(0 10%, 100% 20%, 100% 80%, 0 90%);
             width: 100%;
@@ -40,13 +44,38 @@ const StyledHero = Styled.section`
                 max-width: 500px;
             }
         }
+    }
+
+    .foods {
+        padding-top: 50px;
+        padding-bottom: 50px;
+    
+        .foods-content-container {
+            position: relative;
+
+            .img-fluid {
+                max-width: 500px;
+                margin-top: -100px;
+            }
+            
+            .foods-decoration-top,
+            .foods-decoration-bottom {
+                position: absolute;
+            }
+
+            .foods-decoration-top {
+                top: -100px;
+                right: -230px;
+                max-width: 400px;
+            }
+
+            .foods-decoration-bottom {
+                bottom: 50px;
+                left: 0;
+                max-width: 700px;
+            }
+        }
+    }
 `
 
-const StyledFood = Styled.section`
-    background: red;
-`
-
-export {
-    StyledHero,
-    StyledFood
-}
+export default StyledFestival
